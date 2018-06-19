345a7c35fc5669868303365dbe59a30ab459056d
uenaka.yusuke <uenaka.yusuke@tis.co.jp>
2018-06-19 21:10:38 +0900

    Getkomitto

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -5,6 +5,22 @@ public class App {
 		for (int i = 0, l = args.length; i < l; i++) {
 			String output = String.format("argv[%s]: %s", i, args[i]);
 			System.out.println(output);
+		
+		
+			WebTarget target = client.target("http://challenge-server.code-check.io")
+					.path("/api/hash") 
+					.queryParam("q",arg[i]);
+			
+			try {
+			    String result = target.request().get(String.class);
+			    System.out.println(result);
+			} catch (BadRequestException e) {
+			    logger.error("response=" + e.getResponse().readEntity(String.class), e);
+			    throw e;
+			}
+			
+			
+			
 		}
 	}
 }
